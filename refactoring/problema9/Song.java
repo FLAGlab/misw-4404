f32bbdcbc50c46479763a50fccafcf6f7c67ed76
ncardozo <n.cardozo@uniandes.edu.co>
2023-11-09 11:29:59 -0500

    adding problem 9
    Signed-off-by: ncardozo <n.cardozo@uniandes.edu.co>

---
+++ refactoring/problema9/Song.java
@@ -0,0 +1,175 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Song {
+
+	public static void generateSong2Animals() {
+		System.out.println("There was an old lady who swallowed a fly.\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a spider...\n" +
+        "...She's dead, of course!");
+	}
+	
+	public static void generateSong3Animals() {
+		System.out.println("There was an old lady who swallowed a fly.\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a spider;\n" +
+        "That wriggled and wiggled and tickled inside her.\n" +
+        "She swallowed the spider to catch the fly;\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a bird...\n" +
+        "...She's dead, of course!");
+	}
+	
+	public static void generateSong4Animals() {
+		System.out.println("There was an old lady who swallowed a fly.\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a spider;\n" +
+        "That wriggled and wiggled and tickled inside her.\n" +
+        "She swallowed the spider to catch the fly;\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a bird;\n" +
+        "How absurd to swallow a bird.\n" +
+        "She swallowed the bird to catch the spider,\n" +
+        "She swallowed the spider to catch the fly;\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a cat...\n" +
+        "...She's dead, of course!");
+	}
+
+	public static void generateSong5Animals() {
+		System.out.println("There was an old lady who swallowed a fly.\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a spider;\n" +
+        "That wriggled and wiggled and tickled inside her.\n" +
+        "She swallowed the spider to catch the fly;\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a bird;\n" +
+        "How absurd to swallow a bird.\n" +
+        "She swallowed the bird to catch the spider,\n" +
+        "She swallowed the spider to catch the fly;\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a cat;\n" +
+        "Fancy that to swallow a cat!\n" +
+        "She swallowed the cat to catch the bird,\n" +
+        "She swallowed the bird to catch the spider,\n" +
+        "She swallowed the spider to catch the fly;\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a dog...\n" +
+        "...She's dead, of course!");
+	}
+	
+	public static void generateSong6Animals() {
+		System.out.println("There was an old lady who swallowed a fly.\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a spider;\n" +
+        "That wriggled and wiggled and tickled inside her.\n" +
+        "She swallowed the spider to catch the fly;\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a bird;\n" +
+        "How absurd to swallow a bird.\n" +
+        "She swallowed the bird to catch the spider,\n" +
+        "She swallowed the spider to catch the fly;\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a cat;\n" +
+        "Fancy that to swallow a cat!\n" +
+        "She swallowed the cat to catch the bird,\n" +
+        "She swallowed the bird to catch the spider,\n" +
+        "She swallowed the spider to catch the fly;\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a dog;\n" +
+        "What a hog, to swallow a dog!\n" +
+        "She swallowed the dog to catch the cat,\n" +
+        "She swallowed the cat to catch the bird,\n" +
+        "She swallowed the bird to catch the spider,\n" +
+        "She swallowed the spider to catch the fly;\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a cow...\n" +
+        "...She's dead, of course!");
+	}
+	
+	public static void generateSong7Animals() {
+		System.out.println("There was an old lady who swallowed a fly.\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a spider;\n" +
+        "That wriggled and wiggled and tickled inside her.\n" +
+        "She swallowed the spider to catch the fly;\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a bird;\n" +
+        "How absurd to swallow a bird.\n" +
+        "She swallowed the bird to catch the spider,\n" +
+        "She swallowed the spider to catch the fly;\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a cat;\n" +
+        "Fancy that to swallow a cat!\n" +
+        "She swallowed the cat to catch the bird,\n" +
+        "She swallowed the bird to catch the spider,\n" +
+        "She swallowed the spider to catch the fly;\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a dog;\n" +
+        "What a hog, to swallow a dog!\n" +
+        "She swallowed the dog to catch the cat,\n" +
+        "She swallowed the cat to catch the bird,\n" +
+        "She swallowed the bird to catch the spider,\n" +
+        "She swallowed the spider to catch the fly;\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "There was an old lady who swallowed a cow;\n" +
+        "\n"+
+        "I don't know how she swallowed a cow!\n" +
+        "She swallowed the cow to catch the dog,\n" +
+        "She swallowed the dog to catch the cat,\n" +
+        "She swallowed the cat to catch the bird,\n" +
+        "She swallowed the bird to catch the spider,\n" +
+        "She swallowed the spider to catch the fly;\n" +
+        "I don't know why she swallowed a fly - perhaps she'll die!\n" +
+        "\n" +
+        "There was an old lady who swallowed a horse...\n" +
+        "...She's dead, of course!");
+	}
+	
+    public static void main(String[] args) {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	System.out.println("Enter the number of anumals you want to make a song with:");
+    	try {
+			int num = Integer.parseInt(br.readLine());
+			if(num == 2)
+				Song.generateSong2Animals();
+			else if(num == 3)
+				Song.generateSong3Animals();
+			else if(num == 4)
+				Song.generateSong4Animals();
+			else if(num == 5)
+				Song.generateSong5Animals();
+			else if(num == 6)
+				Song.generateSong6Animals();
+			else if(num == 7)
+				Song.generateSong7Animals();
+			else
+				System.out.println("Not enough animals, or too many");
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+}
